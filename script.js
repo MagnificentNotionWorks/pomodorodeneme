f3255ee4c97afcd5b5acf44e75f2083219fdac23
MagnificentNotionWorks <154951663+MagnificentNotionWorks@users.noreply.github.com>
2024-01-03 20:17:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //import React from "https://cdn.skypack.dev/react@17.0.1";
-//import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 
 const SESSION = "Session";
 const BREAK = "Break";
